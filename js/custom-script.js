dd8b061bfdd5c86b88b1a62be5569fe28094ce73
Atlas <atlas@example.com>
2025-09-25 22:53:57 +0300

    Updates

---
+++ js/custom-script.js
@@ -0,0 +1,1 @@
+jQuery(document).ready((function(){$((function(){$(".typed").typed({stringsElement:$(".typed-strings"),typeSpeed:100,backDelay:500,loop:!0,contentType:"html",loopCount:!1,callback:function(){},resetCallback:function(){newTyped()}})}))})),jQuery(document).ready((function(){document.body.style.removeProperty("height")}));
